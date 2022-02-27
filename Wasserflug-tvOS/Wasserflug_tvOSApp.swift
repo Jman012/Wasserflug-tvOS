4a16e9fe89361eef23064726b74e512e5822bc6e
James Linnell <james.n.linnell@gmail.com>
2022-02-26 16:28:44 -0800

    Initial commit: Xcode tvOS app template

---
+++ Wasserflug-tvOS/Wasserflug_tvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wasserflug_tvOSApp.swift
+//  Wasserflug-tvOS
+//
+//  Created by James Linnell on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Wasserflug_tvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
