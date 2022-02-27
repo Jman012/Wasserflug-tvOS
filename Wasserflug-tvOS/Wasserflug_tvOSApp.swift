f095b4ef4cd6a86ad3fb719bc61e448fecfd7826
James Linnell <james.n.linnell@gmail.com>
2022-02-26 16:30:20 -0800

    Convert tvOS template files from spaces to tabs.

--- Wasserflug-tvOS/Wasserflug_tvOSApp.swift
+++ Wasserflug-tvOS/Wasserflug_tvOSApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct Wasserflug_tvOSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
