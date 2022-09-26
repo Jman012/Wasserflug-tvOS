ab3161e9350d5eafe5a74043efd75737ff157982
James Linnell <james.n.linnell@gmail.com>
2022-09-26 12:54:40 -0700

    Fix issue with logging in after the api updates, about the captcha token

--- FloatplaneAPIClient/FloatplaneAPIClient/Classes/OpenAPIs/Models/AuthLoginV2Request.swift
+++ FloatplaneAPIClient/FloatplaneAPIClient/Classes/OpenAPIs/Models/AuthLoginV2Request.swift
@@ -15,10 +15,10 @@ public struct AuthLoginV2Request: Content, Hashable {
 
     public var username: String
     public var password: String
-    /** The Google Recaptcha v2/v3 token to verify the request. On web browsers, this is required. For mobile or TV applications, this is not required only if the User-Agent indicates so (e.g., if the User-Agent contains \"CFNetwork\" in its value). Otherwise, the application would have to supply a valid captcha token, which can be difficult to obtain dynamically in some scenarios. */
+    /** The Google Recaptcha v2/v3 token to verify the request. On web browsers, this is required. For mobile or TV applications, this is not required only if the User-Agent indicates so (e.g., if the User-Agent contains \"CFNetwork\" in its value). Otherwise, the application would have to supply a valid captcha token, which can be difficult to obtain dynamically in some scenarios. In this case, this should be undefined (no key), not null. */
     public var captchaToken: String?
 
-    public init(username: String, password: String, captchaToken: String?) {
+    public init(username: String, password: String, captchaToken: String? = nil) {
         self.username = username
         self.password = password
         self.captchaToken = captchaToken
@@ -36,7 +36,7 @@ public struct AuthLoginV2Request: Content, Hashable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(username, forKey: .username)
         try container.encode(password, forKey: .password)
-        try container.encode(captchaToken, forKey: .captchaToken)
+        try container.encodeIfPresent(captchaToken, forKey: .captchaToken)
     }
 }
 
